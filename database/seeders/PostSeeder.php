477410fef97288c0753dd5f0af2e3eeddbea7790
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-04-02 12:32:36 -0600

    todo listo hasta la migracion de seeder con error

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Image;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $posts = Post::factory(100)->create();
+
+        foreach ($posts as $post) {
+            Image::factory(1)->create([
+                'imageable_id' => $post->id,
+                'imageable_type' => Post::class
+            ]);
+        }
+    }
+}
