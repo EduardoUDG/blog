388398878b72e39a54674304cbf7437dbccd16d1
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-04-09 10:40:08 -0500

    adminLTE instalado

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,19 @@
-prueba
+@extends('adminlte::page')
+
+@section('title', 'Blog admin')
+
+@section('content_header')
+    <h1>Blog admin</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
