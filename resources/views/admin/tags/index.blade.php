8e2fdbfc0bbd8fd60233ba3d6897321fc9f37dac
Yarenden <alejandra.figueroa8818@alumnos.udg.mx>
2021-04-09 18:22:49 -0500

    Boton de creación de nueva tag

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,45 @@
+@extends('adminlte::page')
+
+@section('title', 'Blog admin')
+
+@section('content_header')
+    <a class="btn btn-secondary btn-sm float-right"href="{{route('admin.tags.create')}}">Nueva etiqueta</a>
+
+    <h1>Mostrar listado de etiqueta</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($tags as $tag)
+                    <tr>
+                        <td>{{$tag->id}}</td>
+                        <td>{{$tag->name}}</td>
+                        <td width="10px">
+                            <a class="btn btn-primary btn-sm" href="{{route('admin.tags.edit', $tag)}}">Editar </a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{route('admin.tags.destroy', $tag)}}" method="POST">
+                                    @csrf
+                                    @method('delete')
+
+                                    <button class="btn btn-danger btn-sm"type="submit">Eliminar</button>
+                                </form>
+                            </td>
+                    </tr>
+
+                    @endforeach
+                </tbody>
+        </div>
+    </div>
+@stop
--- routes/admin.php
+++ routes/admin.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\TagController;
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
 
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 
+Route::resource('tag', 'UserController');
+
+Route::resource('tags', TagController::class)->names('admin.tags');
