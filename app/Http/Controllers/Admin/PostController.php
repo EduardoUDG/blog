42182995e8bdff5c488ea8505c150a0381d529a3
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-04-26 10:03:12 -0500

    arreglando sintaxis de asignacion masiva del modelo post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -45,6 +45,7 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
+        
         $post = Post::create($request->all());
 
         if ($request->tags) {
