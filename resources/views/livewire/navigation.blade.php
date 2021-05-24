9ad08b6ea8433993087c4c8a4da2fd32d77d51b5
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-05-24 15:45:57 -0500

    cache del proyecto y logo

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -32,8 +32,9 @@
 
             {{-- Logotipo --}}
             <a href="/" class="flex-shrink-0 flex items-center">
-                <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
-                <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                {{-- <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
+                <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"> --}}
+                <h1 class="text-white font-extrabold">BlogCUCEI</h1>
             </a>
 
             {{-- Menu Lg --}}
